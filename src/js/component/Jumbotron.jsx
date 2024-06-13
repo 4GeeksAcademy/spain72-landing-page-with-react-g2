724c1e423f087f3b5b099d589d87a26928e95e57
Stefano Imperial Salvador <105392252+is-stefano@users.noreply.github.com>
2024-06-13 20:58:28 +0100

    Revert "Revert "feat: Jumbotron finalizado""

---
+++ src/js/component/Jumbotron.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+
+export const NavbJumbotron = () => {
+    return (
+        <>
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">A WARM WELCOME</h1>
+                <p className="lead">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perferendis ex expedita possimus placeat obcaecati, commodi distinctio debitis quia totam deserunt eaque consequatur, quibusdam accusamus soluta ad, sint et? Explicabo, molestiae.</p>
+                <br />
+                    <p className="lead">
+                        <a className="btn btn-primary btn-lg" href="#" role="button">Call to action!</a>
+                    </p>
+            </div>
+        </div>
+        </>
+    )
+}
