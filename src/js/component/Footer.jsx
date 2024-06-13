67fb3d0f6da8d6d6adad314387180ad488ea93d2
Esteban Briceño Peroza <162641471+00000Esteban1111@users.noreply.github.com>
2024-06-13 19:38:18 +0000

    Footer listo

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+export const Footer = () => {
+
+    return (
+        <footer className="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top  bg-dark">
+            <div className="col mb-3 text-center text-light ">
+                <a href="/" className="d-flex align-items-center justify-content-center mb-3 link-body-emphasis text-decoration-none">
+                    {/* Aquí puedes añadir un logo o icono si lo necesitas */}
+                </a>
+                <p className="text-body-secondary">copyrigh© 2024</p>
+            </div>
+
+            <div className="col mb-3"></div>
+
+
+      
+    </footer >
+        
+    )
+}
